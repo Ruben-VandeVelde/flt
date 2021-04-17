6f827e5ebec7d7d14742e9a00ceb234571936300
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2021-04-17 19:27:24 +0200

    Cleanup flt.lean.

--- src/flt.lean
+++ src/flt.lean
@@ -3,60 +3,59 @@ import data.int.parity
 import data.nat.gcd
 import data.pnat.basic
 import tactic
+import number_theory.fermat4
 import .primes
 import .odd_prime_or_four
 
 lemma flt_three
-  (a b c : ℤ) :
+  {a b c : ℤ} (ha : a ≠ 0) (hb : b ≠ 0) (hc : c ≠ 0) :
   a ^ 3 + b ^ 3 ≠ c ^ 3 :=
 begin
   sorry,
 end
 
 lemma flt_four
-  (a b c : ℤ) :
+  {a b c : ℤ} (ha : a ≠ 0) (hb : b ≠ 0) (hc : c ≠ 0) :
   a ^ 4 + b ^ 4 ≠ c ^ 4 :=
-begin
-  sorry,
-end
+not_fermat_4 ha hb
 
 lemma flt_primes
-  (p : ℕ) (a b c : ℤ) (h : 5 ≤ p) (hp : p.prime) :
+  {p : ℕ} {a b c : ℤ} (h : 5 ≤ p) (hp : p.prime) (ha : a ≠ 0) (hb : b ≠ 0) (hc : c ≠ 0) :
   a ^ p + b ^ p ≠ c ^ p :=
 begin
---  let frey := sorry, --y ^ 2 = x * ( x - a ^ p ) * ( x + b ^ p )
+  -- by wiles
   sorry,
 end
 
 lemma flt_composite
-  (p n : ℕ) (a b c : ℤ) (h : p ∣ n) (g : a ^ n + b ^ n = c ^ n):
-  ∃ a' b' c' : ℤ, a' ^ p + b' ^ p = c' ^ p :=
+  {p n : ℕ} {a b c : ℤ} (h : p ∣ n) (ha : a ≠ 0) (hb : b ≠ 0) (hc : c ≠ 0)
+  (g : a ^ n + b ^ n = c ^ n):
+  ∃ a' b' c' : ℤ, a' ≠ 0 ∧ b' ≠ 0 ∧ c' ≠ 0 ∧ a' ^ p + b' ^ p = c' ^ p :=
 begin
-  obtain ⟨m, hm⟩ := h,
-  subst hm,
+  obtain ⟨m, rfl⟩ := h,
   rw [mul_comm, pow_mul, pow_mul, pow_mul] at g,
-  use [a ^ m, b ^ m, c ^ m],
-  exact g,
+  exact ⟨a ^ m, b ^ m, c ^ m, pow_ne_zero _ ha, pow_ne_zero _ hb, pow_ne_zero _ hc, g⟩,
 end
 
-theorem flt (n : ℕ) (a b c : ℤ) (h : 2 < n) : a ^ n + b ^ n ≠ c ^ n :=
+theorem flt {n : ℕ} {a b c : ℤ} (h : 2 < n) (ha : a ≠ 0) (hb : b ≠ 0) (hc : c ≠ 0) :
+  a ^ n + b ^ n ≠ c ^ n :=
 begin
   by_contradiction H,
   push_neg at H,
   zify at h,
   obtain ⟨p, hdvd, hp⟩ := odd_prime_or_four.exists_and_dvd h,
   rw [int.coe_nat_dvd_right] at hdvd,
-  obtain ⟨a', b', c', hcomp⟩ := flt_composite p.nat_abs n a b c hdvd H,
+  obtain ⟨a', b', c', ha', hb', hc', hcomp⟩ := flt_composite hdvd ha hb hc H,
   cases hp,
   { subst hp,
-    apply flt_four a' b' c' hcomp },
+    exact flt_four ha' hb' hc' hcomp },
   { obtain ⟨hp, hodd⟩ := hp,
     by_cases h3 : p.nat_abs = 3,
     { rw h3 at hcomp,
-      apply flt_three a' b' c' hcomp },
+      apply flt_three ha' hb' hc' hcomp },
     { rw int.prime_iff_nat_abs_prime at hp,
       rw [←int.nat_abs_odd, nat.odd_iff_not_even] at hodd,
-      apply flt_primes p.nat_abs a' b' c' _ hp hcomp,
+      refine flt_primes _ hp ha' hb' hc' hcomp,
       by_contradiction hcon,
       push_neg at hcon,
       have := hp.two_le,
