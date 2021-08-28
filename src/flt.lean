da8391ab35ac57ab4320945a60b4de79c30ff3fd
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2021-08-28 14:10:44 +0200

    Introduce Frey curve.

--- src/flt.lean
+++ src/flt.lean
@@ -1,3 +1,4 @@
+import algebraic_geometry.EllipticCurve
 import data.int.basic
 import data.int.parity
 import data.nat.gcd
@@ -17,6 +18,21 @@ lemma flt_primes
   {p : ℕ} {a b c : ℤ} (h : 5 ≤ p) (hp : p.prime) (ha : a ≠ 0) (hb : b ≠ 0) (hc : c ≠ 0) :
   a ^ p + b ^ p ≠ c ^ p :=
 begin
+  intro H,
+  let disc : ℚ := 16 * (a ^ p * b ^ p * c ^ p) ^ 2,
+  have disc_ne_zero : disc ≠ 0,
+  { dsimp [disc],
+    refine mul_ne_zero (by norm_num) (pow_ne_zero 2 (mul_ne_zero (mul_ne_zero _ _) _));
+    apply pow_ne_zero; assumption_mod_cast },
+  let frey : EllipticCurve ℚ :=
+  { a1 := 0,
+    a2 := b ^ p - a ^p,
+    a3 := 0,
+    a4 := - (a ^ p * b ^ p),
+    a6 := 0,
+    disc_unit := units.mk0 _ disc_ne_zero,
+    disc_unit_eq := by {
+      rw [←@int.cast_inj ℚ, int.cast_pow] at H, simp [EllipticCurve.disc_aux, ←H], ring } },
   -- by wiles
   sorry,
 end
